3b1629126161a3f85de145b442769cae4c973760
Luciano <lualgoritmo@outlook.com>
2022-08-27 17:44:13 -0300

    Implementando arquitetura

--- AgendaFinanceira/app/src/main/java/com/luciano/agendafinanceira/ui/home/view/homeActivity.kt
+++ AgendaFinanceira/app/src/main/java/com/luciano/agendafinanceira/ui/home/view/homeActivity.kt
@@ -3,7 +3,7 @@ package com.luciano.agendafinanceira
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class homeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
