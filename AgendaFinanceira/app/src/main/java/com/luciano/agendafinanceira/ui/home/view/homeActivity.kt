73bb3f15ac20b9136d00c81106ca6c5bd4179b4e
Luciano <lualgoritmo@outlook.com>
2022-08-27 18:52:00 -0300

    Implementando Layout

--- AgendaFinanceira/app/src/main/java/com/luciano/agendafinanceira/ui/home/view/homeActivity.kt
+++ AgendaFinanceira/app/src/main/java/com/luciano/agendafinanceira/ui/home/view/homeActivity.kt
@@ -2,10 +2,14 @@ package com.luciano.agendafinanceira
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luciano.agendafinanceira.databinding.ActivityMainBinding
 
 class homeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
